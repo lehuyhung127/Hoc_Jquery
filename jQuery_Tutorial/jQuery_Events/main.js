0d76296868c3b3465e1cf39ba985a39ef048e809
Huy Hung <huyhungsp2003@gmail.com>
2024-03-08 09:17:35 +0700

    phuong thuc on

--- jQuery_Tutorial/jQuery_Events/main.js
+++ jQuery_Tutorial/jQuery_Events/main.js
@@ -13,4 +13,24 @@ $(document).ready(function () {
   $(".dbl-click").dblclick(function () {
     alert("bye");
   });
+
+  //    3. khi di chuột tới sự kiện thì sự kiện sẽ thay đổi
+  $(".mouseenter").mouseenter(function () {
+    $(this).css("color", "blue");
+  });
+
+  // 4. khi chuột rời khỏi sự kiện thì sự kiện sẽ thay đổi
+  $(".mouseleave").mouseleave(function () {
+    $(this).css("color", "red");
+  });
+
+  // 5. khi hover vafo class se doi mau
+  $(".hovers").hover(function () {
+    $(this).css("color", "green");
+  });
+
+  // 6. phương thức on: sử lí nhiều sự kiện cũng 1 lúc
+  $(".method-on").on({
+    
+  });
 });
