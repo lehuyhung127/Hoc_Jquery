1186e33af7a55758cab495737beb5bdf5a2db949
Huy Hung <huyhungsp2003@gmail.com>
2024-03-11 10:57:59 +0700

    Add New Context HTML: append, prepend

--- jQueryHTML/jQuery_Add/main.js
+++ jQueryHTML/jQuery_Add/main.js
@@ -1,2 +1,11 @@
 // GET CONTENT: text(), html(), val()
-$(document).ready(function () {});
+$(document).ready(function () {
+  // append
+  $("#btn_append").click(function () {
+    $("#append_p").append("<b> 'Hiển thị ở cuối phần tử'</b>");
+  });
+
+  $("#btn_prepend").click(function () {
+    $("#pre_p").prepend("<b>'Thêm vào đâu phần tử được chọn'</b> ");
+  });
+});
