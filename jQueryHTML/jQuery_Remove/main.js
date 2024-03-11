4df28ceba113b6a4d272c4c82e32d55099bd8ffc
Huy Hung <huyhungsp2003@gmail.com>
2024-03-11 12:13:59 +0700

    feat: update remove width claass

--- jQueryHTML/jQuery_Remove/main.js
+++ jQueryHTML/jQuery_Remove/main.js
@@ -9,4 +9,15 @@ $(document).ready(function () {
   $("#btn_empty").click(function () {
     $("#div2").empty();
   });
+
+  // remove all elements with class="test": xóa toàn bộ thẻ p có class="test"
+  $(".btn_removeClass").click(function () {
+    $("p").remove(".test");
+  });
+
+  // removes all <p> elements with class="test" and class="demo" 
+  // xóa các thẻ có class="test" và class="demo"
+  $(".btn_remove2Class").click(function () {
+    $("p").remove(".test, .demo")
+  }) 
 });
